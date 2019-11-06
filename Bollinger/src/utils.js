b7768810d2c627abe8106041e32f01645d4ef1ce
gassde <gassde@ecs.csus.edu>
2019-11-05 22:29:18 -0800

    almost

--- Bollinger/src/utils.js
+++ Bollinger/src/utils.js
@@ -18,7 +18,7 @@ function parseData(parse) {
 const parseDate = timeParse("%Y-%m-%d");
 
 export function getData() {
-	const promiseMSFT = fetch("https://cdn.rawgit.com/rrag/react-stockcharts/master/docs/data/MSFT.tsv")
+	const promiseMSFT = fetch("https://cdn.jsdelivr.net/gh/lazertooth/BollingerView@master/Bollinger/data/PCG.tsv")
 		.then(response => response.text())
 		.then(data => tsvParse(data, parseData(parseDate)))
 	return promiseMSFT;
